da04b23e07bbca04a9d95728671c5f3160775dc9
AM-darka <amanda.kempf@gmail.com>
2025-09-07 11:41:52 -0400

    Added comments to the 'french fry' project!

--- topics/function-calls-module/js/script.js
+++ topics/function-calls-module/js/script.js
@@ -1,22 +1,26 @@
 /**
- * Function Calls
+ * Poorly Portraid Cold Fries 
  * Amanda Stormyr
  * 
- * Working on the function calls examples! 
+ * An invitation to examine one’s own reflections 
+ * when encountering poorly executed art. 
+ * This is an introspective experience!
  */
 
 "use strict";
 
-
+//* Creates the canvas for our still-life anti-masterpiece */
 function setup() {
+    // create canvas in the shape of long countertop
     createCanvas(1100, 677);
 }
 
-
+//* Drawing of french fries on a granite countertop */ 
 function draw() {
+    // The grey granite countertop
     background(180, 180, 180);
 
-    //french fries
+    // The yellow french fries
     noStroke();
     fill(250, 250, 0)
     rect(100, 100, 420, 50);
