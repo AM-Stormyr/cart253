a7e11c4cd23601d1f2820670c987bc73dc49788c
AM-darka <amanda.kempf@gmail.com>
2025-09-26 14:25:20 -0400

    Started selfportrait using sketch.js and inserted image!
    it works!!

--- topics/art-jam/js/script.js
+++ topics/art-jam/js/script.js
@@ -11,7 +11,13 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+// let pic1
+// function preload() {
+//     pic1 = loadImage(/Users/amandaleakempf / Documents / GitHub / cart253 / topics / art - jam / assets / images / amandaEinstein.png)
+// }
+
 function setup() {
+    createCanvas(1920, 1080);
 
 }
 
@@ -20,5 +26,7 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(125, 0, 0)
+    // image(pic1, 1000, 500, 1000, 700)
 
 }
---
+++ topics/self-portrait/sketch.js
@@ -0,0 +1,31 @@
+/**
+ * Doggy and Me
+ * AM Stormyr
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+
+let pic1;
+
+// Load the image.
+function preload() {
+  pic1 = loadImage('/assets/images/amandaSmall.png');
+}
+
+
+
+
+function setup() {
+  createCanvas(1430, 768);
+}
+
+function draw() {
+  background(250, 200, 120);
+  image(pic1, 250, 105, 826, 664)
+}
