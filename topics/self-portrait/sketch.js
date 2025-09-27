4934d9cd3d95186130024fea50f2015acdfb90f0
AM-darka <amanda.kempf@gmail.com>
2025-09-27 18:44:50 -0400

    Option to respond 'YES' or 'NO' soundfile will play according to answer!
    We're interacting!!

--- topics/self-portrait/sketch.js
+++ topics/self-portrait/sketch.js
@@ -7,7 +7,7 @@
 
 
 /**
- * I can't wait to see what this will be!
+ * This is a simulation of meeting in the street. 
 */
 
 
@@ -25,6 +25,10 @@ let ohHey;
 let somethingFun;
 let ohHeyFinished = false;
 let showTextBox = false;
+let imBored;
+let letsGo;
+let somethingFunFinished = false;
+let showYesNoBoxes = false;
 
 //Loading Cursor
 let pinkCursor;
@@ -41,6 +45,8 @@ function preload() {
   dogPant = loadSound('assets/sounds/dog-panting.mp3')
   ohHey = loadSound('assets/sounds/oh-hey.mp3')
   somethingFun = loadSound('assets/sounds/something-fun.mp3')
+  imBored = loadSound('assets/sounds/im-bored.mp3')
+  letsGo = loadSound('assets/sounds/letsGo.mp3')
 
   pinkCursor = loadImage('assets/images/pink-cursor.png');
 }
@@ -80,7 +86,7 @@ function mousePressed() {
     dogPant.pan(-0.4)
 
 
-    ohHey.setVolume(0.12);
+    ohHey.setVolume(0.14);
     ohHey.pan(0.4)
     ohHey.play(1)
 
@@ -99,13 +105,46 @@ function mousePressed() {
     // Check if mouse is over the text box area
     if (mouseX > 308 && mouseX < 558 && mouseY > 270 && mouseY < 420) {
       somethingFun.play();
-      somethingFun.setVolume(0.2);
+      somethingFun.setVolume(0.28);
       somethingFun.pan(0.4);
       showTextBox = false;
       console.log("Text box clicked, playing next sound");
+      // Set up callback for when somethingFun finishes
+      somethingFun.onended(function () {
+        somethingFunFinished = true;
+        showYesNoBoxes = true;
+        console.log("somethingFun finished, showing yes/no boxes");
+      });
+
+      console.log("Text box clicked, playing somethingFun");
+    }
+  }
+  // Third click - handle yes/no choices
+  else if (somethingFunFinished && showYesNoBoxes) {
+    // Check if clicking "NO" box (left box)
+    if (mouseX > 250 && mouseX < 400 && mouseY > 270 && mouseY < 370) {
+      imBored.play();
+      imBored.setVolume(0.299);
+      showYesNoBoxes = false;
+
+      // When im-bored finishes, show yes/no boxes again
+      imBored.onended(function () {
+        showYesNoBoxes = true;
+        console.log("im-bored finished, showing yes/no boxes again");
+      });
+
+      console.log("NO clicked, playing im-bored");
+    }
+    // Check if clicking "YES" box (right box)
+    else if (mouseX > 450 && mouseX < 600 && mouseY > 270 && mouseY < 370) {
+      letsGo.play();
+      letsGo.setVolume(0.3);
+      showYesNoBoxes = false;
+      console.log("YES clicked, playing letsGo");
     }
   }
 }
+
 
 
 
@@ -153,9 +192,44 @@ function draw() {
     fill(100, 50, 200);
     textAlign(RIGHT);
     textSize(22);
-    text("Not much. You?", 267, 319, 245);
+    text("Not much! You?", 280, 319, 245);
     pop();
   }
+
+  // Show yes/no boxes after somethingFun finishes
+  if (showYesNoBoxes && somethingFunFinished) {
+    // NO box (left)
+    push();
+    noStroke();
+    fill(0, 0, 0); // black for NO
+    rect(250, 270, 150, 100);
+    pop();
+
+    push();
+    textFont('Press Start 2P');
+    fill(255, 255, 255);
+    textAlign(CENTER);
+    textSize(20);
+    text("NO", 325, 330);
+    pop();
+
+    // YES box (right)
+    push();
+    noStroke();
+    fill(244, 194, 194); //pink for yes
+    rect(450, 270, 150, 100);
+    pop();
+
+    push();
+    textFont('Press Start 2P');
+    fill(255, 255, 255);
+    textAlign(CENTER);
+    textSize(20);
+    text("YES", 525, 330);
+    pop();
+  }
+
+
 
 
 
