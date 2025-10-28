d9a4ace8bb2c03ed3ea5e742821ccf2d6624aa14
AM-darka <amanda.kempf@gmail.com>
2025-10-28 12:54:26 -0400

    Finished the speech interface!

--- topics/arrays/speech-speech/js/script.js
+++ topics/arrays/speech-speech/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Speech! Speech!
+ * Amanda Stormyr
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Interactive speech playing interface! 
  */
 
 "use strict";
@@ -11,14 +10,45 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+//The speech itself
+const speech = ["Hi", "how", "are", "you?"];
+
+//which sentence in the speech to display
+let speechIndex = 0;
+
+
 function setup() {
+    createCanvas(600, 100);
+}
+
+/**
+ * Displays the text
+ */
+function draw() {
+
+    background(0);
+
+    // Get the current line of the speech
+    let currentLine = speech[speechIndex];
+
+    // Display the line
+    push();
+    fill(255);
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width / 2, height / 2);
+    pop();
 
 }
 
+function mousePressed() {
+    //next line
+    speechIndex = speechIndex + 1;
+    // handle speech
+    if (speechIndex >= speech.length) {
+        //Start over
+        speechIndex = 0;
+    }
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
 
 }
