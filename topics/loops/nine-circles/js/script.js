2a4ec42274544df54c5cbd7e5dc3d3b915b72807
AM-darka <amanda.kempf@gmail.com>
2025-11-02 15:36:12 -0500

    started nine circles!

---
+++ topics/loops/nine-circles/js/script.js
@@ -0,0 +1,39 @@
+/**
+ * Vertical circles
+ * Pippin Barr
+ * 
+ * Draws a series of circles from the top to the bottom of the canvas.
+ * Arguably not in the most efficient way.
+ */
+
+"use strict";
+
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(400, 400);
+}
+
+/**
+ * Draw circles from the top to the bottom of the canvas
+ */
+function draw() {
+    background(0);
+
+
+
+    // Draw a series of 50-pixel diameter circles
+    // Starting at the top of the canvas
+    // And ending at the bottom
+    let x = 200;
+    let y = 0;
+    let diameter = 50;
+
+    for (i = 0; i < 9; i++) {
+
+        ellipse(x, y, diameter);
+        y += diameter;
+    }
+
+}
