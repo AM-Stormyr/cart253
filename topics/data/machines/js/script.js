2d6af11a1804c0900b78474ab54c57b1a0957970
AM-darka <amanda.kempf@gmail.com>
2025-11-11 12:28:36 -0500

    Created constants and drawmachine variables!

--- topics/data/machines/js/script.js
+++ topics/data/machines/js/script.js
@@ -10,6 +10,31 @@
 
 "use strict";
 
+
+const incinerator = {
+    x: 0,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#ff4400"
+}
+
+const freezer = {
+    x: 150,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#bbbbff"
+}
+
+const crusher = {
+    x: 300,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#777777"
+}
+
 /**
  * Create the canvas
  */
@@ -23,24 +48,38 @@ function setup() {
 function draw() {
     background(0);
 
-    // Incinerator
-    push();
-    noStroke();
-    fill("#ff4400");
-    rect(0, 100, 100, 100);
-    pop();
+    drawMachine(incinerator);
+    drawMachine(freezer);
+    drawMachine(crusher);
 
-    // Freezer
-    push();
-    noStroke();
-    fill("#bbbbff");
-    rect(150, 100, 100, 100);
-    pop();
+}
 
-    // Crusher
+function drawMachine(machine) {
     push();
     noStroke();
-    fill("#777777");
-    rect(300, 100, 100, 100);
+    fill(machine.fill);
+    rect(machine.x, machine.y, machine.width, machine.height);
     pop();
 }
+
+
+// // Incinerator
+// push();
+// noStroke();
+// fill("#ff4400");
+// rect(0, 100, 100, 100);
+// pop();
+
+// // Freezer
+// push();
+// noStroke();
+// fill("#bbbbff");
+// rect(150, 100, 100, 100);
+// pop();
+
+// // Crusher
+// push();
+// noStroke();
+// fill("#777777");
+// rect(300, 100, 100, 100);
+// pop();
