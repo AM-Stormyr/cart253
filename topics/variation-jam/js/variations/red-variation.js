27f5d2b80fdcf9c89c74b6e3f0a560861846f0d5
AM-darka <amanda.kempf@gmail.com>
2025-12-06 13:38:10 -0500

    noise background for Dormant (variation 1). looks like spores or film grain. I like it. Could also be a microscope-vibe!

--- topics/variation-jam/js/variations/red-variation.js
+++ topics/variation-jam/js/variations/red-variation.js
@@ -7,16 +7,53 @@
 /**
  * This will be called just before the red variation starts
  */
-function redSetup() {
 
+let noiseTexture;
+
+
+function redSetup() {
+    generateNoiseTexture();
+}
+
+function generateNoiseTexture() {
+    noiseTexture = createGraphics(width, height);
+    noiseTexture.loadPixels();
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < height; y++) {
+
+            // Base grain - shifted lighter
+            let grain = random(180, 240);  // bumped up brightness
+
+            // Perlin shadows - softened + lighter
+            let p = noise(x * 0.01, y * 0.01) * 50 - 25;
+
+            // Combine
+            let c = constrain(grain + p, 0, 255);
+
+            noiseTexture.set(x, y, color(c));
+        }
+    }
+    noiseTexture.updatePixels();
+
+    // Speckles: more white “dust”
+    noiseTexture.noStroke();
+    for (let i = 0; i < 2000; i++) {
+        let x = random(width);
+        let y = random(height);
+        noiseTexture.fill(255, 50);  // translucent bright specks
+        noiseTexture.rect(x, y, 1, 1);
+    }
 }
 
 /**
  * This will be called every frame when the red variation is active
  */
 function redDraw() {
-    background("red");
+    image(noiseTexture, 0, 0);
+
 }
+
 
 /**
  * This will be called whenever a key is pressed while the red variation is active
